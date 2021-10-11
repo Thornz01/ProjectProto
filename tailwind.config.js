b269b163c9c8c5881d1fae65d4c1f1671f37927b
Thornz01 <thornzgithub01@gmail.com>
2021-10-11 11:18:22 +0800

    Initial import

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+  purge: ["./**/*.html",
+  "./*.html",
+  "./**/*.js",],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      'desktop': '1280px',
+      'tablet': '640px',
+      'phone': '375px',
+    },
+    extend: {},
+    colors:{
+      "sidebar" : "#E8E8E8;",
+      "second" : "#F5F5F5;",
+      "white" : "#FFFFFF",
+    },
+    fontFamily:{
+      Redhattext: ["Redhat, sans-serif"],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
